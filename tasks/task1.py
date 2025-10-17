b1362fb636de551e3b3b9a273f81dfee382eb8d2
dobrey383 <nikitakarimov7@gmail.com>
2025-10-17 10:36:59 +0500

    sdelal2

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,7 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    print(a == b == c)
     
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
--- tasks/task4.py
+++ tasks/task4.py
@@ -3,7 +3,10 @@
 def solve():
 # Ниже пишите решение задачи
     
-    
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    print(a + b > c and a + c > b and b + c > a)
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
